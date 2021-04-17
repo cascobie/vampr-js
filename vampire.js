e4459ce0d84e0431a01aebdbbf208ae2b7edfe8e
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-04-16 19:42:30 -0600

    first draft?

--- vampire.js
+++ vampire.js
@@ -10,24 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
-
+  
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -38,6 +46,34 @@ class Vampire {
   closestCommonAncestor(vampire) {
 
   }
+  
+  //--------------DEPTH FIRST TRAVERSAL------------------------------------------------------------
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let vampires = [];
+
+    if (this.yearConverted >= 1980) {
+      vampires.push(this)
+    }
+
+    for (const offsp of this.offspring) {
+      const millenialOffspring = offsp.allMillennialVampires;
+      vampires = vampires.concat(millenialOffspring);
+    }
+
+    return vampires;
+  }
+
 }
 
 module.exports = Vampire;
